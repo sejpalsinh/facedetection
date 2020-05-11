ab90081543641381da3a1aa8b0b8cc18fa05e76f
sejpalsinh jadeja <59164285+sejpalsinh@users.noreply.github.com>
2020-05-11 16:38:07 +0530

    Add files via upload

---
+++ facedetect.py
@@ -0,0 +1,13 @@
+import cv2
+face_cascade = cv2.CascadeClassifier('rku_facedetection.xml')
+cap = cv2.VideoCapture(0)
+while True:
+    _, img = cap.read()
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+    for (x, y, w, h) in faces:
+        cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+    cv2.imshow('img', img)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+cap.release()
